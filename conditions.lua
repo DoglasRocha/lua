32376bcdaafc59eac39b8b33e10717b2cc257a3e
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-07-04 15:50:35 -0300

    conditions

---
+++ conditions.lua
@@ -0,0 +1,11 @@
+a = 10
+
+if (a == 11) then
+	print("a eh igual a 11")
+elseif (a < 10) then
+	print("a eh menor que 10")
+else 
+	print("a n eh nem 11 nem menor que 10")
+	print("valor de a: ", a)
+end
+
